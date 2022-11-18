edac881d9902aad21b87a5f123fbc7a226c06832
owlsoverjericho <owlsoverjericho@protonmail.com>
2022-11-19 00:46:13 +0200

    added esling and prettier

--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 import ACTIONS from "../front/src/Socket/actions.mjs";
 import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT || 3003;
 const HTTPserver = createServer();
 const io = new Server(HTTPserver, {
@@ -14,15 +14,15 @@ const io = new Server(HTTPserver, {
 });
 
 const getRooms = () => {
-    const {rooms} = io.sockets.adapter;
-    return Array.from(rooms.keys())
-}
+    const { rooms } = io.sockets.adapter;
+    return Array.from(rooms.keys());
+};
 
 const shareRooms = () => {
     io.emit("share-rooms", {
         rooms: getRooms(),
-    })
-}
+    });
+};
 
 io.on("connection", (socket) => {
     shareRooms();
@@ -32,36 +32,39 @@ io.on("connection", (socket) => {
             socket.join(data.roomID);
             socket.emit("created", data.roomID);
             console.log("room created");
-        }
-        else if (myRoom.size === 1) {
+        } else if (myRoom.size === 1) {
             socket.join(data.roomID);
             socket.emit("joined", data.roomID);
             console.log("another user joined the room");
-        }
-        else {
+        } else {
             socket.emit("full", data.roomID);
             console.log("the room is full");
         }
         console.log(io.sockets.adapter.rooms.get(data.roomID));
     });
-    socket.on("ready", data => {
+    socket.on("ready", (data) => {
         socket.broadcast.to(data.roomID).emit("ready");
     });
-    socket.on("candidate", event => {
+    socket.on("candidate", (event) => {
         socket.broadcast.to(event.roomID).emit("candidate", event);
     });
-    socket.on("offer", event => {
+    socket.on("offer", (event) => {
         socket.broadcast.to(event.roomID).emit("offer", event.sdp);
     });
-    socket.on("answer", event => {
+    socket.on("answer", (event) => {
         socket.broadcast.to(event.roomID).emit("answer", event.sdp);
     });
-    socket.on('connect_failed', (e) => {
+    socket.on("connect_failed", (e) => {
         console.log(`CONNECTION ERROR: ${e}`);
     });
     socket.on("chat-message", (data) => {
-        io.in(data.roomID).emit("chat-message", { message: data.message, userID: socket.id });
+        io.in(data.roomID).emit("chat-message", {
+            message: data.message,
+            userID: socket.id,
+        });
     });
     console.log(socket.rooms);
 });
-HTTPserver.listen(PORT, () => console.log(`HTTP server is running on port ${PORT} 🚀`));
+HTTPserver.listen(PORT, () =>
+    console.log(`HTTP server is running on port ${PORT} 🚀`)
+);
